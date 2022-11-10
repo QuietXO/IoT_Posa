cb0109a89328da5176aaa55d68963d62d820f593
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-10 10:56:12 +0100

    Web Fix

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 
 	echo '<h2>Pages:</h2>';
 
-	<a href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">
+	echo <a href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">
 	Web Page with Parameters</a>
 ?>
--- index2.php
+++ index2.php
@@ -1,14 +1,14 @@
 <?php
         echo '<h1>index2.php web page with parameters</h1>';
 
-        echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?a=10&b=2 </h2>';
-        
+        echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2 </h2>';
+
         $sn1 = $_GET["A"];
         $sn2 = $_GET["B"];
-        
+
         $text = "A = " . $sn1 . " B = " . $sn2;
         $sum = $sn1 + $sn2;
-        
+
         echo $text;
         echo "<br>";
         echo "Sum: " . $sum;
