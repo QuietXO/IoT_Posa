7ee1522fa6342ad0704273000aeaee7ec799a1d6
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-10 11:05:29 +0100

    Test

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 
 	echo '<h2>Pages:</h2>';
 
-	echo '<a href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">
-	Web Page with Parameters</a>';
+	echo '<p href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">
+	Web Page with Parameters</p>';
 ?>
--- index2.php
+++ index2.php
@@ -1,7 +1,10 @@
 <?php
         echo '<h1>index2.php web page with parameters</h1>';
 
-        echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2 </h2>';
+        echo '<h1>href="https://phpipaiot.azurewebsites.net">
+      	Home Page</h1>';
+
+        echo '<p> https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2 </p>';
 
         $sn1 = $_GET["A"];
         $sn2 = $_GET["B"];
