081057244b42073a4b301550142b2ff926307ff1
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-10 11:14:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 	echo '<h2>Pages:</h2>';
 
-	echo '<p href="https://eheeey.azurewebsites.net/index2.php?A=10&B=2">
-	Web Page with Parameters</p>';
+	echo '<a href="https://eheeey.azurewebsites.net/index2.php?A=10&B=2">
+	Web Page with Parameters</a>';
 	//echo '<p href="https://eheeey.azurewebsites.net/index3.php?A=10&B=2">
 	//Web Page with Parameters</p>';
 ?>
