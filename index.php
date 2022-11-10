44bee5322052a109bf7d86f6ca7e23ee26d1ded5
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-10 11:11:33 +0100

    Web Page Visual update

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
 	echo '<h2>Pages:</h2>';
 
-	echo '<p href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">
+	echo '<p href="https://eheeey.azurewebsites.net/index2.php?A=10&B=2">
 	Web Page with Parameters</p>';
+	//echo '<p href="https://eheeey.azurewebsites.net/index3.php?A=10&B=2">
+	//Web Page with Parameters</p>';
 ?>
--- index2.php
+++ index2.php
@@ -1,10 +1,10 @@
 <?php
         echo '<h1>index2.php web page with parameters</h1>';
 
-        echo '<h1>href="https://phpipaiot.azurewebsites.net">
+        echo '<h1>href="https://eheeey.azurewebsites.net">
       	Home Page</h1>';
 
-        echo '<p> https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2 </p>';
+        echo '<p> https://eheeey.azurewebsites.net/index2.php?A=10&B=2 </p>';
 
         $sn1 = $_GET["A"];
         $sn2 = $_GET["B"];
