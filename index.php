52b25d7b81f9967d3d8e104e6961ac1c62344753
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-10 10:19:27 +0100

    test update

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 	echo '<h1>First web on Azure</h1>';
 
-	echo '<h2>Pages</h2>';
+	echo '<h2>Pages:</h2>';
 
-	<a href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">Test Page 1</a> 
+	<a href="https://phpipaiot.azurewebsites.net/index2.php?A=10&B=2">
+	Web Page with Parameters</a>
 ?>
--- index2.php
+++ index2.php
@@ -1,5 +1,7 @@
 <?php
         echo '<h1>index2.php web page with parameters</h1>';
+
+        echo '<h2> https://phpipaiot.azurewebsites.net/index2.php?a=10&b=2 </h2>';
         
         $sn1 = $_GET["A"];
         $sn2 = $_GET["B"];
