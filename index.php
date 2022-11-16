462454327b4fccca3dd8d78bde90233c4d23b527
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-16 22:07:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,15 @@
 			<form class="support-page" action="contact.php" method="post">
 				<input type="text" name="name" placeholder="Full Name">
 				<input type="text" name="mail" placeholder="Your e-mail">
+				<div class="elem-group">
+	    		<label for="department-selection">What seems to be the problem:</label>
+	    		<select id="department-selection" name="concerned_department" required>
+		        <option value="">Select a Department</option>
+		        <option value="billing">Billing</option>
+		        <option value="marketing">Marketing</option>
+		        <option value="technical support">Technical Support</option>
+	    		</select>
+  			</div>
 				<input type="text" name="subject" placeholder="Subject">
 				<textarea name="message" placeholder="Your Message"></textarea>
 				<button type="submit" name="submit">SEND MAIL</button>
