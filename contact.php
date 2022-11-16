10cbebcf93b7b617133e3bc8a381f457ffdf1f1b
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-16 21:44:40 +0100

    test

--- contact.php
+++ contact.php
@@ -1,6 +1,14 @@
-<?php
-        echo '<h1>Support Page</h1>';
-
-        echo '<h2><a href="https://eheeey.azurewebsites.net">
-        Home Page</a></h2>';
-?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Support Page</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700"
+    rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <main>
+      <p>Send Mail</p>
+    </main>
+  </body>
+</html>
