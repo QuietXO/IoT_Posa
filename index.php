693e27e6b8b23fc23291c14c464f9121c832bd21
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-11-17 08:15:23 +0100

    visual update

--- index.php
+++ index.php
@@ -1,37 +1,28 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Main Page</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700"
-    rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <main>
-			<div id="header">
-      	<h1>Support Page</h1>
-			</div>
-			<form class="support-page" action="contact.php" method="post">
-				<p>Your Name:</p>
-				<input type="text" name="name1" placeholder="First Name">
-				<input type="text" name="nameL" placeholder="Last Name">
-				<p>Your Email:</p>
-				<input type="text" name="mail" placeholder="Your e-mail">
-				<div class="elem-group">
-	    		<label for="department-selection">What seems to be the problem:</label>
-	    		<select id="department-selection" name="concerned_department" required>
-		        <option value="">Select a Department</option>
-		        <option value="billing">Billing</option>
-		        <option value="marketing">Marketing</option>
-		        <option value="technical support">Technical Support</option>
-	    		</select>
-  			</div>
-				<p>Message Subject:</p>
-				<input type="text" name="subject" placeholder="Subject">
-				<p>Message:</p>
-				<textarea name="message" placeholder="Your Message"></textarea>
-				<button type="submit" name="submit">SEND MAIL</button>
-			</form>
-    </main>
-  </body>
+</head>
+<body>
+    <div class="container">
+        <h1>Contact Me</h1>
+        <p>Feel free to contact us and we will get back to you as soon as we can.</p>
+        <form action="contact.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="subject">Subject:</label>
+            <input type="text" name="subject" id="subject">
+            <label for="message">Message</label>
+            <textarea name="message" cols="30" rows="10"></textarea>
+            <input type="submit" value="Send">
+        </form>
+    </div>
+</body>
 </html>
