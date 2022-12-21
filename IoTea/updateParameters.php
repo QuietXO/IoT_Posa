dc30a33a84fbdaff02c664d427947055db3a3374
QuietXO <74719452+QuietXO@users.noreply.github.com>
2022-12-21 21:15:50 +0100

    ...

--- IoTea/updateParameters.php
+++ IoTea/updateParameters.php
@@ -1,22 +1,37 @@
 <?php
 
-$parameters = [
-    'hint',
-    'name',
-    'type',
-    'tPlaced',
-    'heater',
-    'tReady',
-    'minFluid',
-    'valve',
-    'temperature'
-];
+// nacitame globalne nastavenia
+require_once("init.php");
 
-$parametersJson = [];
-foreach ($parameters as $parameter) {
-    $parametersJson[$parameter] = isset($_GET[$parameter]) ? $_GET[$parameter] : null;
+// funkcia zisti, ci ide o prve vlozenie dat po (re)starte ESP
+// ak mame vsetky parametre, ide o prve/vstupne data
+function isInitialUpdate(array $keys)
+{
+    foreach ($keys as $key) {
+        if (!isset($_GET[$key])) {
+            return false;
+        }
+    }
+    return true;
 }
 
-$fp = fopen('parameters.json', 'w');
-fwrite($fp, json_encode($parametersJson));
-fclose($fp);
+// nacitame data zo suboru alebo vytvorime nove pole
+if (file_exists($filename) && !isInitialUpdate($keys)) {
+    $parametersJson = getData();
+    $parametersJson[PARAM_UPDATED_AT] = $datetime;
+} else {
+    $parametersJson = [
+        PARAM_CREATED_AT => $datetime,
+        PARAM_UPDATED_AT => null,
+    ];
+}
+
+// updatneme parametre
+foreach ($keys as $key) {
+    if (isset($_GET[$key])) {
+        $parametersJson[$key] = $_GET[$key];
+    }
+}
+
+// ulozime updatnute parametre do suboru
+setData($parametersJson);
